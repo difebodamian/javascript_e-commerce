65db5f0a16c584f75d930f907c33db0b8df82da4
Damián Di Febo <difebodamian@gmail.com>
2022-07-30 07:56:47 -0300

    librerias

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
 /*
-Poder ver el precio final del carrito  
-Agregar la funcionalidad de poder subir o bajar la cantidad de un producto
 Darle la posibilidad al usuario de pagar en cuotas
 Agregar un botón en el nav para cambiar los precios de los games a dólares o pesos
-Que se sumen los totales de los precios al carrito
-Mostrar el price final del carrito
-agreagar sweet alert cuando compre el carrito, y cuando quiera vaciarlo (dandole una opción de arrepentirse al usuario)}
-toast para mensaje por cada juego que agregue al carrito
 */
-
-
-//Agregar funcionalidad de agregar el precio en pesos
 
 //  VARIABLES
 
-// cargamos las variables
 const carrito = document.querySelector(".cart");
 const cancelarCompraBtn = document.querySelector(".cancel-buy-btn");
 const listaDeJuegos = document.querySelector(".games");
@@ -43,6 +33,7 @@ class Videojuego {
     }
 }
 // array de los objetos 
+
 const arrayDeJuegos = [
     new Videojuego("Resident Evil Village", "Terror", 4299, 1),
     new Videojuego("Gta V", "Acción - Mundo Abierto", 1199, 2), 
@@ -54,10 +45,12 @@ const arrayDeJuegos = [
     new Videojuego("The Quarry", "Drama", 5999, 8) 
 ]
 
-//
+// verificaciones
+
 verificarEstadoDelCarrito();
 verificarSesionIniciada();
-// cargan todos los eventListeners
+
+// cargar los event listeners
 
 cargarEventListener()
 function cargarEventListener() {
@@ -75,6 +68,11 @@ function cargarEventListener() {
     cerrarSesionBtn.addEventListener("click", cerrarSesion)
 }
 
+
+
+
+// función que toma los datos que ingresa el usuario
+
 function formulario(e) {
     e.preventDefault();
     const nombre = inputNombre.value;
@@ -86,6 +84,8 @@ function formulario(e) {
     (nombre,email,contraseña) ? usuario(nombre, email, contraseña) : console.log("error");
 }
 
+// se agregar en un objeto los datos que ingresó el usuario en el formulario
+
 function usuario(nombre,email,contraseña) {
     const usuario = {
         nombre: nombre,
@@ -95,29 +95,33 @@ function usuario(nombre,email,contraseña) {
     agregarUsuarioAlLocalStorage(usuario)
 }
 
+// se agrega el usuario al local storage
+
 function agregarUsuarioAlLocalStorage(usuario) {
     const usuarioNuevo = JSON.stringify(usuario);
     localStorage.setItem("user", usuarioNuevo);
     console.log(`${usuario.nombre} ha sido ingresado al local storage`);
     ocultarFormulario();
     mostrarBotonDeCerrarSesion();
+    toastifyIniciarSesion(usuario);
 }
+
+// funciones para manipular la visibilidad del formulario y el botón para cerrar sesión
 
 function ocultarFormulario() {
     contenedorFormulario.classList.add("none");
 }
-
 function mostrarFormulario() {
     contenedorFormulario.classList.contains("none") & contenedorFormulario.classList.remove("none") 
 }
-
 function mostrarBotonDeCerrarSesion() {
     cerrarSesionBtn.classList.contains("none") & cerrarSesionBtn.classList.remove("none");
 }
-
 function ocultarBotonCerrarSesion() {
     cerrarSesionBtn.classList.add("none");
 }
+
+// función que verifica si hay un usuario existente y lo elimina
 
 function cerrarSesion() {
     ocultarBotonCerrarSesion();
@@ -126,6 +130,8 @@ function cerrarSesion() {
     user & localStorage.removeItem("user");
     console.log("sesion cerrada");
 }
+
+// verifica si hay un usuario registrado en el local storage
 
 function verificarSesionIniciada() {
     const usuarioExistente = localStorage.getItem("user");
@@ -137,7 +143,7 @@ function verificarSesionIniciada() {
 
 // FUNCIONES 
 
-//esta función llama a la función readGameData, que esta agrega un producto al array y al html por consiguiente mediante callbacks
+// esta función, mediante callbacks, agrega un juego al carrito y al local storage
 
 function añadirJuegoAlCarrito(e) {
     e.preventDefault();
@@ -150,10 +156,11 @@ function añadirJuegoAlCarrito(e) {
         });
         leerDatosDelJuego(juegoSeleccionado);
         agregarAlLocalStorage(juegoSeleccionado);
+        toastifyAgregar(juegoSeleccionado);
     }
 }
 
-// esta función elimina un producto del carrito html y del array de productos
+// esta función elimina un producto del carrito y del local storage, y modifica el precio total
 
 function eliminarJuegoDelCarrito(e) {
     e.preventDefault();
@@ -168,10 +175,12 @@ function eliminarJuegoDelCarrito(e) {
         localStorage.setItem("carrito", arrayDeLocalStorage);
         articulosDelCarrito = articulosDelCarrito.filter((juego) => juego.id !== juegoID);
         carritoHTML();
+        toastifyEliminar(nombreDelJuego);
     }
     sumarPrecios()
 }
 
+// modifica la cantidad de juegos en el carrito mediante los botones + y -
 
 function modificarCantidad(e) {
     e.preventDefault(); 
@@ -183,6 +192,7 @@ function modificarCantidad(e) {
         });
         leerDatosDelJuego(juegoSeleccionado);
         agregarAlLocalStorage(juegoSeleccionado);
+        toastifyAgregar(juegoSeleccionado)
     } else if (clase.contains("reducir-cantidad")) {
         const juegoID = parseInt(e.target.getAttribute("id"));
         const juego = articulosDelCarrito.find((juego) => juego.id === juegoID);
@@ -190,8 +200,11 @@ function modificarCantidad(e) {
         const arrayNuevo = carritoDelLocalStorage.filter(juego => juego.id !== juegoID);
         const arrayDeLocalStorage = JSON.stringify(arrayNuevo);
         localStorage.setItem("carrito", arrayDeLocalStorage);
+        toastifyEliminar(juego.nombre)
         juego.cantidad--;
         if (juego.cantidad === 0) {
+            const nombreDelJuego = juego.nombre;
+            toastifyEliminar(nombreDelJuego)
             articulosDelCarrito = articulosDelCarrito.filter((juego) => juego.id !== juegoID);
             carritoHTML();
         }
@@ -199,12 +212,15 @@ function modificarCantidad(e) {
     actualizarCarrito()
 }
 
+// verifica si hay un carrito existente en el local storage, si no hay lo crea y lo setea
 
 function verificarLocalStorage() {
     const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
     carrito ? console.log("hay productos almacenados en el local storage") : localStorage.setItem("carrito", carrito);
     return carrito
 }
+
+// verifica si hay juegos agregados en el carrito del local storage, si hay los muestra
 
 function verificarEstadoDelCarrito() {
     const carrito = verificarLocalStorage();
@@ -214,6 +230,7 @@ function verificarEstadoDelCarrito() {
     })
 }
 
+// agrega un juego al local storage
 
 function agregarAlLocalStorage(juego) {
     const arrayDelLocalStorage = verificarLocalStorage()
@@ -225,7 +242,7 @@ function agregarAlLocalStorage(juego) {
 
 
 // esta función lee la info del producto, define si existe o no en el carrito
-// si existe le modifica la cantidad, si no llama a la función cartHTML
+// si existe le modifica la cantidad, si no lo agrega al carrito
 
 function leerDatosDelJuego(juego) {
     const infoDelJuego = {
@@ -252,8 +269,7 @@ function leerDatosDelJuego(juego) {
     actualizarCarrito();
 }
 
-// hacer una función que me saque todos los precios de los juegos que tiene el carrito y me cree
-// un nuevo array con esos items
+// sumar los precios del carrito y lo muestra en pantalla
 
 function sumarPrecios() {
     const resultado = articulosDelCarrito.reduce((acc,juego) => {
@@ -262,10 +278,7 @@ function sumarPrecios() {
     resultadoTotal.textContent = "$" + resultado;
 }
 
-
-
-// esta función limpia el html primero y después recorre el array de productos creando un nuevo div por cada producto que querramos agregar
-// en el que le muestra sus propiedades y agrega un botón para poder eliminar ese producto posteriormente
+// agrega un div por cada juego que se agrega al carrito
 
 function carritoHTML() {
     
@@ -293,13 +306,20 @@ function mostrarCarrito() {
     carrito.classList.add("cart-style")
 }
 
+// vacía el html del carrito
+
 function limpiarHTML() {
     carrito.innerHTML = "";
 }
+
+// actualiza el carrito y su precio
+
 function actualizarCarrito() {
     carritoHTML();
     sumarPrecios();
 }
+
+// llama a los productos que están en el local storage y los vuelve a setear vacíos, vacía el carrito de html y lo actualiza
 
 function vaciarCarrito() {
     let localStorageArray = JSON.parse(localStorage.getItem("carrito"));
@@ -311,6 +331,10 @@ function vaciarCarrito() {
     actualizarCarrito()
     console.log("Local Storage vacío");
 }
+
+// LIBRERIAS    
+
+// uso de la librería sweet alert para vaciar el carrito
 
 function vaciarCarritoSweetAlert() {
     const swalWithBootstrapButtons = Swal.mixin({
@@ -349,6 +373,56 @@ function vaciarCarritoSweetAlert() {
       })
 }
 
+// toast para indicar que agregué un juego al carrito
 
+function toastifyAgregar(juego) {
+    Toastify({
+        text: `Has agregado ${juego.nombre} a tu carrito`,
+        duration: 3000,
+        destination: "#carrito",
+        newWindow: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+}
+
+// toast para indicar que elimino un juego del carrito
+function toastifyEliminar(juego) {
+    Toastify({
+        text: `Has eliminado ${juego} de tu carrito`,
+        duration: 3000,
+        destination: "#carrito",
+        newWindow: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+}
+
+function toastifyIniciarSesion(usuario) {
+    Toastify({
+        text: `Bienvenido ${usuario.nombre}`,
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+}
 
 
